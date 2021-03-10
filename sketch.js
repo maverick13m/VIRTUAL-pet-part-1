76e1544257fb24246825369e35176a92d8afb841
maverick13m <71173739+maverick13m@users.noreply.github.com>
2021-03-10 16:11:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var database;
+var dog,dogImage,happyDog,treats,treatStock
+
+function preload(){
+  dogImage = loadImage("images/dogImg.png")
+  dogHappy = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+	createCanvas(500, 500);
+  database = firebase.database()
+  dog=createSprite(250,300,50,50)
+  dog.addImage(dogImage)
+  dog.scale =0.3
+  treatStock = database.ref('food')
+  treatStock.on("value",readStock,showError)
+}
+
+function draw() { 
+  background(46,139,87)
+  if(treats !=undefined){
+    if(keyWentDown(UP_ARROW)){
+      console.log(treats)
+      writeStock()
+      dog.addImage(dogHappy)
+      console.log(treats)
+    }
+  }
+
+  drawSprites();
+
+  textSize(20)
+  fill("blue")
+  text("TREATS left "+treats,200,100)
+  text("press UP_ARROW to feed DEKU :)",100,50)
+}
+
+function readStock(data){
+  treats = data.val()
+
+}
+
+function writeStock(){
+
+  if(treats<=0){
+    treats =0;
+  }
+  else{
+  treats -=1
+  }
+  database.ref('/').update({food:treats})
+}
+
+function showError(err){
+  console.log("error"+err)
+}
